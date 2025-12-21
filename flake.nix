85c43bb3210b25c30aab1bad6f8c0092eece17fd
Emilio López <emilio.lopez@trailofbits.com>
2025-12-21 00:31:25 -0300

    redo flake

--- flake.nix
+++ flake.nix
@@ -15,164 +15,72 @@
         "aarch64-linux"
         "aarch64-darwin"
       ];
+      compiler-nix-name = "ghc984";
     in
       flake-utils.lib.eachSystem supportedSystems (system:
       let
-        overlays = [ haskellNix.overlay
-          (final: prev: let
-            # Override libff to remove unnecessary boost dependency
-            libff = prev.libff.overrideAttrs (oldAttrs: {
-              # Apply Windows compatibility patch for clock_gettime
-              patches = (oldAttrs.patches or []) ++ prev.lib.optionals prev.stdenv.hostPlatform.isWindows [
-                ./nix/libff-mingw.patch
-              ];
-            });
-            # if we pass a library folder to ghc via --extra-lib-dirs that contains
-            # only .a files, then ghc will link that library statically instead of
-            # dynamically (even if --enable-executable-static is not passed to cabal).
-            # we use this trick to force static linking of some libraries on macos.
-            stripDylib = drv : pkgs.runCommand "${drv.name}-strip-dylibs" {} ''
-              mkdir -p $out
-              mkdir -p $out/lib
-              cp -r ${drv}/* $out/
-              rm -rf $out/**/*.dylib
-            '';
-
-            # this is not perfect for development as it hardcodes solc to 0.5.7, test suite runs fine though
-            # 0.5.7 is not available on aarch64 darwin so alternatively pick 0.8.5
-            solc = solc-pkgs.mkDefault pkgs (pkgs.solc_0_5_7 or pkgs.solc_0_8_5);
-
-
-            # Static fixup function that takes ncurses as parameter
-            makeStaticFixup = ncurses-static: let
-              grep = "${pkgs.gnugrep}/bin/grep";
-              perl = "${pkgs.perl}/bin/perl";
-              otool = "${pkgs.darwin.binutils.bintools}/bin/otool";
-              install_name_tool = "${pkgs.darwin.binutils.bintools}/bin/install_name_tool";
-              codesign_allocate = "${pkgs.darwin.binutils.bintools}/bin/codesign_allocate";
-              codesign = "${pkgs.darwin.sigtool}/bin/codesign";
-            in if pkgs.stdenv.isLinux
-            then ''
-              # fix TERMINFO path in ncurses
-              ${perl} -i -pe 's#(${ncurses-static}/share/terminfo)#"/etc/terminfo:/lib/terminfo:/usr/share/terminfo:/usr/lib/terminfo" . "\x0" x (length($1) - 65)#e' $out/bin/echidna
-              chmod 555 $out/bin/echidna
-            '' else if pkgs.stdenv.isDarwin then ''
-              # get the list of dynamic libs from otool and tidy the output
-              libs=$(${otool} -L $out/bin/echidna | tail -n +2 | sed 's/^[[:space:]]*//' | cut -d' ' -f1)
-              # get the path for libcxx
-              cxx=$(echo "$libs" | ${grep} '^/nix/store/.*/libc++\.')
-              cxxabi=$(echo "$libs" | ${grep} '^/nix/store/.*/libc++abi\.')
-              iconv=$(echo "$libs" | ${grep} '^/nix/store/.*/libiconv\.')
-              # rewrite /nix/... library paths to point to /usr/lib
-              chmod 777 $out/bin/echidna
-              ${install_name_tool} -change "$cxx" /usr/lib/libc++.1.dylib $out/bin/echidna
-              ${install_name_tool} -change "$cxxabi" /usr/lib/libc++abi.dylib $out/bin/echidna
-              ${install_name_tool} -change "$iconv" /usr/lib/libiconv.dylib $out/bin/echidna
-              # fix TERMINFO path in ncurses
-              ${perl} -i -pe 's#(${ncurses-static}/share/terminfo)#"/usr/share/terminfo" . "\x0" x (length($1) - 19)#e' $out/bin/echidna
-              # check that no nix deps remain
-              nixdeps=$(${otool} -L $out/bin/echidna | tail -n +2 | { ${grep} /nix/store -c || test $? = 1; })
-              if [ ! "$nixdeps" = "0" ]; then
-                echo "Nix deps remain in redistributable binary!"
-                exit 255
-              fi
-              # re-sign binary
-              CODESIGN_ALLOCATE=${codesign_allocate} ${codesign} -f -s - $out/bin/echidna
-              chmod 555 $out/bin/echidna
-            '' else null;
-  
-            echidnaProject = { staticBuild ? false, targetPkgs ? pkgs }:
-              let
-                # Use the target package set for static libraries when cross-compiling
-                pkgsForStatic = targetPkgs;
-                
-                # Static libraries using the correct package set
-                ncurses-static = pkgsForStatic.ncurses.override { enableStatic = true; };
-                staticLibsForTarget = with pkgsForStatic; [
-                  (gmp.override { withStatic = true; })
-                  (libff.override { enableStatic = true; })
-                  (libffi.overrideAttrs (_: { dontDisableStatic = true; }))
-                  ncurses-static
-                  (secp256k1.overrideAttrs (attrs: {
-                    configureFlags = attrs.configureFlags ++ [ "--enable-static" ];
-                  }))
-                  (zlib.override { static = true; shared = false; })
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            haskellNix.overlay
+            (final: prev: {
+              libff = prev.libff.overrideAttrs (oldAttrs: {
+                # Apply Windows compatibility patch for clock_gettime
+                patches = (oldAttrs.patches or []) ++ prev.lib.optionals prev.stdenv.hostPlatform.isWindows [
+                  ./nix/libff-mingw.patch
                 ];
-                staticFixup = makeStaticFixup ncurses-static;
-              in
-              final.haskell-nix.project' {
-                name = "echidna";
-                src = pkgs.haskell-nix.haskellLib.cleanGit {
-                  name = "echidna";
-                  src = ./.;
-                };
-                compiler-nix-name = "ghc984"; # Version of GHC to use
-                
-                modules = [{
-                  packages.hevm.components.library.libs = pkgs.lib.mkForce (with pkgsForStatic;
-                      [ libff secp256k1 ]);
-                  
-                  packages.echidna.components.exes.echidna = if staticBuild then
-                    {
-                      enableShared = false;
-                      enableStatic = true;
-                      configureFlags = map (drv: "--extra-lib-dirs=${stripDylib drv}/lib") staticLibsForTarget;
-                      postInstall = staticFixup;
-                    } else {};
-                }];
+              });
+            })
+          ];
+          # Also ensure we are using haskellNix config. Otherwise we won't be
+          # selecting the correct wine version for cross compilation.
+          inherit (haskellNix) config;
+        };
 
-                # Tools to include in the development shell
-                shell = {
-                  tools = {
-                    cabal = "latest";
-                    hlint = "latest";
-                    haskell-language-server = "latest";
-                  };
-                  # Non-Haskell shell tools go here
-                  buildInputs = with pkgs; [
-                    solc
-                    slither-analyzer
-                  ];
-                };
-              };
-            in {
-            echidnaProjectShared = echidnaProject { staticBuild = false; };
-            echidnaProjectStatic = echidnaProject { staticBuild = true; };
-            # Cross-compilation projects
-            echidnaProjectCrossMingw = echidnaProject { staticBuild = true; targetPkgs = pkgs.pkgsCross.ucrt64; };
-            echidnaProjectCrossMusl = echidnaProject { staticBuild = true; targetPkgs = pkgs.pkgsCross.musl64.pkgsStatic; };
-          })
-          solc-pkgs.overlay
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        solc = solc-pkgs.mkDefault pkgs (pkgs.solc_0_5_7 or pkgs.solc_0_8_5);
-        sharedFlake = pkgs.echidnaProjectShared.flake {
-          crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
-              p.mingwW64
-            ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-              p.musl64
-            ]);
-        };
-        staticFlake = pkgs.echidnaProjectStatic.flake {
-          crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
-              p.mingwW64
-            ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-              p.musl64
-            ]);
-        };
-        windowsFlake = pkgs.echidnaProjectCrossMingw.flake { };
-        muslFlake = pkgs.echidnaProjectCrossMusl.flake { };
-      in sharedFlake // {
-        legacyPackages = pkgs;
+        echidna = {
+          inherit compiler-nix-name;
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "echidna";
+            src = ./.;
+          };
 
-        packages = sharedFlake.packages // {
-          default = sharedFlake.packages."echidna:exe:echidna";
-          echidna-redistributable = if pkgs.stdenv.hostPlatform.isLinux
-            then muslFlake.packages."echidna:exe:echidna"
-            else staticFlake.packages."echidna:exe:echidna";
-          echidna-windows = windowsFlake.packages."echidna:exe:echidna";
+          modules = [{
+            packages.hevm.components.library.libs = pkgs.lib.mkForce (with pkgs; [ libff secp256k1 ]);
+          }];
+
+          #crossPlatforms = p: (pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 [
+          #  p.ucrt64
+          #]) ++ (pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          #  p.aarch64-multiplatform-musl
+          #  p.musl64
+          #]);
         };
-      });
+
+        hsPkgs = pkgs.haskell-nix.project echidna;
+        #flake = hsPkgs.flake {};
+      in
+        let
+          nativePackages = {
+            packages.echidna = hsPkgs.echidna.components.exes.echidna;
+            defaultPackage = self.packages.${system}.echidna;
+          };
+
+          linuxCrossPackages = let
+            # using aarch64-multiplatform-musl here, gives us fully static binaries.
+            aarch64-musl = pkgs.pkgsCross.aarch64-multiplatform-musl.haskell-nix.project echidna;
+            x86_64-musl = pkgs.pkgsCross.musl64.haskell-nix.project echidna;
+            x86_64-windows = pkgs.pkgsCross.ucrt64.haskell-nix.project echidna;
+          in (pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+            packages.echidna-aarch64-musl = aarch64-musl.echidna.components.exes.echidna;
+            packages.echidna-x86_64-musl = x86_64-musl.echidna.components.exes.echidna;
+            packages.echidna-x86_64-windows = x86_64-windows.echidna.components.exes.echidna;
+          }) // (pkgs.lib.optionalAttrs (system == "aarch64-linux") {
+            packages.echidna-aarch64-musl = aarch64-musl.echidna.components.exes.echidna;
+          });
+        
+        in pkgs.lib.recursiveUpdate nativePackages linuxCrossPackages
+
+      );
 
   # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
